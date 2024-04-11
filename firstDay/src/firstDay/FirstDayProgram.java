6ee8a5d94cafcdb5e36820bd2e97f8036c1d1ad7
Rifat Perween <116188576+rifatperween@users.noreply.github.com>
2024-04-11 19:48:51 +0530

    Day1

---
+++ firstDay/src/firstDay/FirstDayProgram.java
@@ -0,0 +1,21 @@
+package firstDay;
+
+public class FirstDayProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int a=10,b=20;
+    int add;
+    add=a+b;
+    System.out.println("Addition" +add);
+    int x=23,y=10;
+    int sub=x-y;
+    System.out.println(sub);
+    int mul=x*y;
+    System.out.println(mul);
+//    int div=x/y;
+    float div=(float) x/y; //typecasting
+    System.out.println(div);
+	}
+
+}
